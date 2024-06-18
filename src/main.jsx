d121ef056cf59f4109b6ddb7c8df36135a298bc2
muhammadhamzaaamir960 <mhamzaaamir960@gmail.com>
2024-06-18 09:54:21 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import {
   Autopilot,
@@ -79,5 +80,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>
 );
